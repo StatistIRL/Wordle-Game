bf69925a7ed20bc43cf78ffce32463e76e7d4620
StatisIRL <vazsry7@gmail.com>
2023-12-30 16:11:12 +0600

    add database; add redirect in main

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import src.logic_module as lm
 
 def main() -> None:
   dm.welcome()
-  
-  
+  print('To start playing, press Enter\nFor exit 1 and Enter')
+  if input() == '1':
+    exit()
+  else:
+    game()
+
+
+def game():
+  print(1111)
   
 if __name__ == '__main__':
   main()
